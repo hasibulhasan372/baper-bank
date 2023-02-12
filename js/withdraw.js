ff5d2d52343757f5e6617e0dfadbc6d19af0c500
HasibulHasan <hasibulhasan372@gmail.com>
2023-02-12 20:07:31 +0600

    second  commit

--- js/withdraw.js
+++ js/withdraw.js
@@ -7,16 +7,24 @@
     const inputWithdrawElement = document.getElementById('withdraw-input');
     const inputWithdrawElementValueString = inputWithdrawElement.value;
     const inputWithdrawAmount = parseFloat(inputWithdrawElementValueString);
-
     // Empty the Input Field 
     inputWithdrawElement.value = '';
+          // Error message 
+      if(isNaN(inputWithdrawAmount)){
+        alert('please give a number')
+        return;
+    };
+  
+
+    
+
 
             // Step : 3 Call the withdraw Area 
             const withdrawArea = document.getElementById('withdraw-area');
             const previousWithdrawString = withdrawArea.innerText;
             const previousWithdrawTotal = parseFloat(previousWithdrawString);
             
-            // step : 4 Added the Withdraw amount with the Withdraw area 
+          
     
 
     // step : Deducted the withdraw amount with the balance 
@@ -31,9 +39,10 @@
     };
 
     
-        const addedWithdraw = previousWithdrawTotal + inputWithdrawAmount;
-        withdrawArea.innerText = addedWithdraw;
-    
+    const addedWithdraw = previousWithdrawTotal + inputWithdrawAmount;
+    withdrawArea.innerText = addedWithdraw;
+
+      // step : 4 Added the Withdraw amount with the Withdraw area 
 
     const totalBalance = balanceAmount - inputWithdrawAmount;
     balanceCall.innerText = totalBalance; 
@@ -41,5 +50,6 @@
     
 
     // last Step : make the withdraw input empty 
+    
 
 });
